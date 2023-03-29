af2dcd598483553464da7cd3c0ec864a01a8fbec
Mustafa Muhammed <mostafa180848@feng.bu.edu.eg>
2023-03-29 21:25:59 +0200

    update

--- code.java
+++ code.java
@@ -1,16 +1,20 @@
+import java.io.*;
+
 public class code {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        int x=5;
+        if (x==5){
+        }
         if (true) {
 
-        }
-
-        else if (true) {
+        }else{
 
         }
 
-        else {
+        for(int i =0;i<6;i++){
 
         }
+
     }
 }
